88403a90570a8b59a2a539f6f08893337d857b7f
João Monteiro <jcsmonteiro1994@gmail.com>
2023-02-05 21:08:22 +0000

    update pom and change hazelcast cache to caffeine

--- src/main/java/com/myhealth/healthmanagermain/config/CacheConfiguration.java
+++ src/main/java/com/myhealth/healthmanagermain/config/CacheConfiguration.java
@@ -1,29 +1,26 @@
 package com.myhealth.healthmanagermain.config;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import lombok.extern.slf4j.Slf4j;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import javax.annotation.PreDestroy;
-
-@Slf4j
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
 
-    @PreDestroy
-    public void destroy() {
-        log.info("Closing Cache Manager");
-        Hazelcast.shutdownAll();
-    }
-
-    @Bean
-    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-        log.debug("Starting HazelcastCacheManager");
-        return new com.hazelcast.spring.cache.HazelcastCacheManager(hazelcastInstance);
-    }
+  @Bean
+  @Primary
+  public CacheManager cacheManager() {
+    CaffeineCacheManager cacheManager = new CaffeineCacheManager("users", "workouts");
+    cacheManager.setCaffeine(Caffeine.newBuilder()
+        .initialCapacity(200)
+        .maximumSize(500)
+        .weakKeys()
+        .recordStats());
+    return cacheManager;
+  }
 }
