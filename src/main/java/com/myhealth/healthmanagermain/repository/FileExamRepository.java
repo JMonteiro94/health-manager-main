df6f81adfb71edfa624788adc04d991531fd59c2
João Monteiro <jcsmonteiro1994@gmail.com>
2023-02-05 17:23:38 +0000

    remove unused classes

--- src/main/java/com/myhealth/healthmanagermain/repository/FileExamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myhealth.healthmanagermain.repository;
-
-import com.myhealth.healthmanagermain.domain.HealthExam;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileExamRepository extends JpaRepository<HealthExam, Long> {
-}
