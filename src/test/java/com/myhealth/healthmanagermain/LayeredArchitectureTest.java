688dd3db7c2be3ee2b2787863ae569fb56b1de67
João Monteiro <jcsmonteiro1994@gmail.com>
2023-02-17 19:35:06 +0000

    added architecture tests

---
+++ src/test/java/com/myhealth/healthmanagermain/LayeredArchitectureTest.java
@@ -0,0 +1,53 @@
+package com.myhealth.healthmanagermain;
+
+import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "com.myhealth.healthmanagermain", importOptions = DoNotIncludeTests.class)
+public class LayeredArchitectureTest {
+
+  @ArchTest
+  public static final ArchRule domainEnumsPackageRule = classes()
+      .that().resideInAPackage("..enums..")
+      .should().onlyBeAccessed().byAnyPackage("..domain..");
+
+  @ArchTest
+  public static final ArchRule respectsTechnicalArchitectureLayers =
+      layeredArchitecture().consideringAllDependencies()
+          .layer("Root").definedBy("..healthmanagermain..")
+          .layer("Demo").definedBy("..bootstrap..")
+          .layer("Config").definedBy("..config..")
+          .layer("Web").definedBy("..web..")
+          .optionalLayer("Service").definedBy("..service..")
+          .optionalLayer("Domain Service").definedBy("..service.domain..")
+          .layer("Security").definedBy("..security..")
+          .layer("Persistence").definedBy("..repository..")
+          .layer("Domain").definedBy("..healthmanagermain.domain..")
+
+          .whereLayer("Config").mayOnlyBeAccessedByLayers("Root")
+          .whereLayer("Web").mayNotBeAccessedByAnyLayer()
+          .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config", "Demo", "Security")
+          .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Service", "Web")
+          .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service")
+          .whereLayer("Domain")
+          .mayOnlyBeAccessedByLayers("Persistence", "Service", "Security", "Web", "Config")
+
+          .ignoreDependency(belongToAnyOf(HealthmanagermainApplication.class), alwaysTrue())
+          .ignoreDependency(alwaysTrue(), belongToAnyOf(
+              com.myhealth.healthmanagermain.config.Constants.class,
+              com.myhealth.healthmanagermain.config.ApplicationProperties.class
+          ));
+
+  @ArchTest
+  public static final ArchRule cyclesCheck = slices().matching("com.(**)").should()
+      .beFreeOfCycles();
+
+}
